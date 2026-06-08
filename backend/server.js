1b1e8a395d557ffa4205b227e12186fa6790e446
Luis <luis12042003@gmail.com>
2026-06-07 18:44:56 -0600

    render

--- backend/server.js
+++ backend/server.js
@@ -550,3 +550,19 @@ app.post(
   }
 
 )
+/* =========================
+PUERTO
+========================= */
+
+const PORT =
+  process.env.PORT || 3000
+
+app.listen(PORT, () => {
+
+  console.log(
+
+    `🔥 Servidor corriendo en puerto ${PORT}`
+
+  )
+
+})
