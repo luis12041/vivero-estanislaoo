8957d1ecded0a377d2eeb794190a6076fcc3d4fe
Luis <luis12042003@gmail.com>
2026-06-09 21:31:59 -0600

    el de martes empleado12345678912

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,75 @@ import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react'
 
+import { VitePWA }
+  from 'vite-plugin-pwa'
+
 export default defineConfig({
 
-  plugins: [react()],
+  plugins: [
+
+    react(),
+
+    VitePWA({
+
+      registerType:
+        'autoUpdate',
+
+      manifest: {
+
+        name:
+          'Vivero Estanislaoo',
+
+        short_name:
+          'Vivero',
+
+        description:
+          'Sistema de venta y administración de plantas',
+
+        theme_color:
+          '#2e7d32',
+
+        background_color:
+          '#ffffff',
+
+        display:
+          'standalone',
+
+        icons: [
+
+          {
+
+            src:
+              'pwa-192x192.png',
+
+            sizes:
+              '192x192',
+
+            type:
+              'image/png'
+
+          },
+
+          {
+
+            src:
+              'pwa-512x512.png',
+
+            sizes:
+              '512x512',
+
+            type:
+              'image/png'
+
+          }
+
+        ]
+
+      }
+
+    })
+
+  ],
 
   build: {
 
